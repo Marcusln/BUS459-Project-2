fa88b996f535de1381b497174b0bf08d380bc821
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-12 14:51:30 +0200

    update

--- knn.r
+++ knn.r
@@ -1,22 +1,68 @@
 predict.knn=function(x0,x,y,k=3)
-  # x0 = 
-  # x = 
-  # y =
-  # k = number of neighbors
+  # x0 = next y value
+  # x = y value
+  # y = predictor variable
+  # k = number of neighbors to use
 {
-  dist=abs(x0-x) # calculate distance 
-  rank.dist=rank(dist,ties.method="random")
-  ypred=mean(y[rank.dist<=k])
+  # calculate manhattan distance
+  dist = abs(x0-x)
+  # return a vector with rank in terms of being the lowest value
+  # lowest distance is rank.dist[84] which has value 1
+  # if the distance between two or more points are equal,
+  # these values are randomly inserted in the indeces of the values that
+  # are equal
+  rank.dist = rank(dist,ties.method="random")
+  # rank.dist <= k fetches the indeces of the k lowest distances
+  # y[rank.dist<=k] returns the value of the k lowest distances
+  # take the mean of this value
+  ypred = mean(y[rank.dist<=k])  # get the values 
   return(ypred)
 }
 
-predict.knn()
+# create variables
+y.next = 0
+y.curr = 0
+return.knn = 0
+# loop through rows
+for(i in 1:nrow(data1)) {
+  # store 
+  y.next[i] = data1$y[i + 1]
+  y.curr[i] = data1$y[i]
+  return.knn[i] = predict.knn(x0 = y.next, x = y.curr, y = data1$y)
+}
+
+plot(data1$x, data1$y)
+lines(data1$x, return.knn, col="blue")
+
+# predict.knn = predict.knn(x0 = y.next, x = y.curr, y = data1$y)
+
+# C)
+
+# poor hacky solution to fill NA with 0
+# data1$dist[100] = 0
+
+# create variables
+predicted = 0
+loo.se = 0
+i = 0
+# loop through rows
+for(i in i:nrow(data1)) {
+  predicted[i] = predict.knn(x0 = data1$y[i + 1],
+                              x = data1$y[i],
+                              y = data1$y)
+  print(paste0(data1$y[i + 1]))
+  #print(paste0(predicted[i]))
+}
+loo.se
+loo.mse = loo.se / nrow(data1)
+
+
+rank.test = rank(data1$distAbs,ties.method="random")
 
 data1$dist = 0
 for(i in 1:nrow(data1)) {
   data1$dist[i] = data1$y[i + 1] - data1$y[i]
 }
-
 
 
 
--- workbook.r
+++ workbook.r
@@ -3,3 +3,25 @@
 ###
 
 plot(data1$x,data1$y)
+
+require(MASS)
+data(Boston, package='MASS')
+y <- Boston$nox
+x <- Boston$dis
+n <- length(x)
+
+cvs <- rep(0, n)
+for(j in seq(n)){
+  ys <- y[-j]
+  xs <- x[-j]
+  d <- nls(ys ~ A + B * exp(C * xs), start=list(A=0.5, B=0.5, C=-0.5))
+  cvs[j] <- (y[j] - predict(d, data.frame(xs=x[j])))^2
+  print(paste0(j, " of ", n, " finished (", round(j/n*100), "%)"))
+}
+
+plot(y~x, pch=19, col='gray', cex=1.5, xlab='dis', ylab='nox')
+d <- nls(y~ A + B * exp(C * x), start=list(A=0.5, B=0.5, C=-0.5))
+lines(predict(d)[order(x)]~sort(x), lwd=4, col='black')
+usr <- par("usr")
+text(usr[1] + 0.9*(usr[2]-usr[1]), usr[3] + 0.9*(usr[4]-usr[3]), paste("LOO MSE", "=", round(mean(cvs), 5)), pos=2)
+text(usr[1] + 0.9*(usr[2]-usr[1]), usr[3] + 0.8*(usr[4]-usr[3]), paste("MSE", "=", round(mean(resid(d)^2), 5)), pos=2)
